e18cc8c5c2286b638f5350ba7a914e4be1dca78f
boltovnya <bolto@tfwno.gf>
2014-09-29 18:40:31 +0100

    Removed gedit temp file

--- dl.py
+++ dl.py
@@ -49,7 +49,7 @@ else:
         sys.stdout.write("|")
         sys.stdout.flush()
 
-HDR = {'User-Agent': 2'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 \
+HDR = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 \
         (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
        'Accept': 'text/html,application/xhtml+xml,\
         application/xml; q=0.9,*/*;q=0.8'}
